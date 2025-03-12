b76888f535d5a624d7ecdbc347d7d307c2de7728
md-golap <golapmax@gmail.com>
2025-03-12 05:57:09 -1200

    add

--- Authentication-System-copy.php
+++ Authentication-System-copy.php
@@ -1,18 +1,36 @@
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+
+        <input type="text" placeholder="Enter Username" name="USERNAME"><br>
+        <input type="password" name="PASSWORD" id="" placeholder="Enter password"><br>
+        <button value="submit">submit</button>
+    </form>
+</body>
+</html>
 <?php
+
+    $Username = $_POST['USERNAME'];
+    $Password = $_POST['PASSWORD'];
+
+
 
     define("USERNAME", "admin");
     define("PASSWORD", "1234");
 
 
-
-    echo "Enter Username";
-    $Username = readline();
-
-    echo "Enter Password";
-    $Password = readline();
-
-
-    
     if(empty($Username) || empty($Password)){
         echo "User name or Password in blank";
 
